2df519e09f3ae05bd89c8576d36cf94dfbcca359
Martina Vokáčová <martinavokacova@gmail.com>
2022-03-20 17:51:16 +0100

    restart

--- vyvolavac.js
+++ vyvolavac.js
@@ -52,3 +52,42 @@ function tahnoutJmeno() {
     let seznam = document.querySelector('#seznam');
     seznam.innerHTML = tazenaJmena + "<br> Vyzkoušeno: " + tazenaJmena.length
 }
+    
+function tahnoutZnovu()
+{
+    let vyherce = document.querySelector('#vyherka');
+    vyherce.textContent = "";
+    let seznam = document.querySelector('#seznam');
+    seznam.innerHTML = "se mohou stát budoucími"; 
+    console.log(jmena.length)
+    jmena = [
+        "Markéta",
+        "Kateřina",
+        "Júlia",
+        "Lucie",
+        "Aneta",
+        "Veronika",
+        "Petra",
+        "Olga",
+        "Naďa",
+        "Nikola",
+        "Eva",
+        "Anísa",
+        "Terezia",
+        "Diana",
+        "Anna",
+        "Iva",
+        "Denisa",
+        "Zuzana",
+        "Jana",
+        "Martina",
+        "Lenka",
+        "Iveta",
+        "Pavla",
+        "Lucie",
+        "Kamila",
+        "Soňa"
+    ];
+    tazenaJmena = [];
+
+}
